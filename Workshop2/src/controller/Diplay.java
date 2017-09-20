a168ad83e887a2b020f4bfbad90d0a6c6160d467
AndersUOlsson <olsson.anders@protonmail.com>
2017-09-20 11:29:44 +0200

    Update

---
+++ Workshop2/src/controller/Diplay.java
@@ -0,0 +1,16 @@
+package controller;
+
+public class Diplay {
+	
+	
+	
+	public void options(int input) {
+		switch(input) {
+			case 1: createMember();
+		
+		}
+	}
+	
+	public void createMember() {}
+
+}
--- Workshop2/src/view/StartGUI.java
+++ Workshop2/src/view/StartGUI.java
@@ -5,5 +5,9 @@ public class StartGUI {
 	public void welcomeWindow() {
 		System.out.print("Welcome to The jolly pirate");
 	}
+	
+	public void option() {
+		System.out.print("What do u like to do ?");
+	}
 
 }
